e0b15119b394b90f3b4a9012523f0cce4423e29e
LegalmenteEmanuel <emanuel070801@gmail.com>
2024-10-15 14:15:27 -0600

    fix: subir excel 3

--- backend-nest/src/modules/Planilla/beneficio/beneficio.service.ts
+++ backend-nest/src/modules/Planilla/beneficio/beneficio.service.ts
@@ -2,7 +2,7 @@ import { BadRequestException, Injectable, InternalServerErrorException, Logger, 
 import { CreateBeneficioDto } from './dto/create-beneficio.dto';
 import { UpdateBeneficioDto } from './dto/update-beneficio.dto';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { DataSource, Repository } from 'typeorm';
 import { Net_Beneficio } from './entities/net_beneficio.entity';
 import { isUUID } from 'class-validator';
 import * as XLSX from 'xlsx';
@@ -18,109 +18,101 @@ export class BeneficioService {
   constructor(){}
   @InjectRepository(Net_Beneficio)
   private beneficioRepository: Repository<Net_Beneficio>
-  @InjectRepository(net_persona)
-  private personaRepository: Repository<net_persona>;
-  @InjectRepository(net_detalle_persona)
-  private detallePersonaRepository: Repository<net_detalle_persona>;
-  @InjectRepository(Net_Detalle_Beneficio_Afiliado)
-  private detalleBeneficioAfiliadoRepository: Repository<Net_Detalle_Beneficio_Afiliado>;
-  @InjectRepository(Net_Banco)
-  private bancoRepository: Repository<Net_Banco>;
-  @InjectRepository(Net_Persona_Por_Banco)
-  private personaPorBancoRepository: Repository<Net_Persona_Por_Banco>;
+  private dataSource: DataSource
   
   async uploadExcel(file: Express.Multer.File) {
     try {
       const workbook = XLSX.read(file.buffer, { type: 'buffer', cellText: false, cellDates: true, cellNF: true });
       const worksheet = workbook.Sheets[workbook.SheetNames[0]];
       const data = XLSX.utils.sheet_to_json(worksheet, { raw: false, defval: "" });
-      
       for (const row of data) {
-        const dni = row['DNI'] ? row['DNI'].toString().trim() : "";
-        const dniCausante = row['DNI_CAUSANTE'] ? row['DNI_CAUSANTE'].toString().trim() : "";
-        const tipoPersona = parseInt(row['ID_TIPO_PERSONA'], 10);
-        const idBeneficio = parseInt(row['CODIGO_BENEFICIO'], 10);
-        const primerPago = parseFloat(row['PRIMER_PAGO']);
-        const ultimoPago = row['ULTIMO_PAGO'] ? parseFloat(row['ULTIMO_PAGO']) : null;
-        const fechaCalculo = new Date(row['FECHA_CALCULO']);
-        const numeroRentas = parseInt(row['NUMERO_RENTAS'], 10);
-        const montoPorPeriodo = parseFloat(row['MONTO_POR_PERIODO(ORDINARIA)']);
-        const periodoInicio = new Date(row['PERIODO_INICIO']);
-        const codigoBanco = row['CODIGO_BANCO'].toString().trim();
-        const numeroCuenta = row['NUMERO_CUENTA'].toString().trim();
+        await this.dataSource.transaction(async (manager) => {
+          const dni = row['DNI'] ? row['DNI'].toString().trim() : "";
+          const dniCausante = row['DNI_CAUSANTE'] ? row['DNI_CAUSANTE'].toString().trim() : "";
+          const tipoPersona = parseInt(row['ID_TIPO_PERSONA'], 10);
+          const idBeneficio = parseInt(row['CODIGO_BENEFICIO'], 10);
+          const primerPago = parseFloat(row['PRIMER_PAGO']);
+          const ultimoPago = row['ULTIMO_PAGO'] ? parseFloat(row['ULTIMO_PAGO']) : null;
+          const fechaCalculo = new Date(row['FECHA_CALCULO']);
+          const numeroRentas = parseInt(row['NUMERO_RENTAS'], 10);
+          const montoPorPeriodo = parseFloat(row['MONTO_POR_PERIODO(ORDINARIA)']);
+          const periodoInicio = new Date(row['PERIODO_INICIO']);
+          const codigoBanco = row['CODIGO_BANCO'].toString().trim();
+          const numeroCuenta = row['NUMERO_CUENTA'].toString().trim();
 
-        if (!dni || isNaN(tipoPersona)) {
-          throw new BadRequestException("Los datos requeridos no están presentes o no son válidos.");
-        }
-
-        let causantePersona = await this.personaRepository.findOne({ where: { n_identificacion: dniCausante } });
-        let causanteDetallePersona;
-        if (!causantePersona) {
-          const causante = this.personaRepository.create({ n_identificacion: dniCausante });
-          causantePersona = await this.personaRepository.save(causante);
-
-          causanteDetallePersona = this.detallePersonaRepository.create({
-            ID_PERSONA: causantePersona.id_persona,
-            ID_CAUSANTE: causantePersona.id_persona,
-            ID_TIPO_PERSONA: 2,
-          });
-          causanteDetallePersona = await this.detallePersonaRepository.save(causanteDetallePersona);
-        } else {
-          causanteDetallePersona = await this.detallePersonaRepository.findOne({ 
-            where: { 
-              ID_PERSONA: causantePersona.id_persona, 
-              ID_CAUSANTE: causantePersona.id_persona,
-              ID_CAUSANTE_PADRE: null 
-            } 
-          });
-          if (!causanteDetallePersona) {
-            throw new InternalServerErrorException("No se encontró el detalle correspondiente para el DNI_CAUSANTE con ID_CAUSANTE_PADRE = null.");
+          if (!dni || isNaN(tipoPersona)) {
+            throw new BadRequestException("Los datos requeridos no están presentes o no son válidos.");
           }
-        }
 
-        let persona = await this.personaRepository.findOne({ where: { n_identificacion: dni } });
-        if (!persona) {
-          persona = this.personaRepository.create({ n_identificacion: dni });
-          persona = await this.personaRepository.save(persona);
-        }
+          let causantePersona = await manager.findOne(net_persona, { where: { n_identificacion: dniCausante } });
+          let causanteDetallePersona;
+          if (!causantePersona) {
+            const causante = manager.create(net_persona, { n_identificacion: dniCausante });
+            causantePersona = await manager.save(causante);
 
-        const detallePersona = this.detallePersonaRepository.create({
-          ID_PERSONA: persona.id_persona,                  
-          ID_CAUSANTE: causantePersona.id_persona,        
-          ID_CAUSANTE_PADRE: causantePersona.id_persona,  
-          ID_DETALLE_PERSONA: causanteDetallePersona.ID_DETALLE_PERSONA, 
-          ID_TIPO_PERSONA: tipoPersona,                   
+            causanteDetallePersona = manager.create(net_detalle_persona, {
+              ID_PERSONA: causantePersona.id_persona,
+              ID_CAUSANTE: causantePersona.id_persona,
+              ID_TIPO_PERSONA: 2,
+            });
+            causanteDetallePersona = await manager.save(causanteDetallePersona);
+          } else {
+            causanteDetallePersona = await manager.findOne(net_detalle_persona, { 
+              where: { 
+                ID_PERSONA: causantePersona.id_persona, 
+                ID_CAUSANTE: causantePersona.id_persona,
+                ID_CAUSANTE_PADRE: null 
+              } 
+            });
+            if (!causanteDetallePersona) {
+              throw new InternalServerErrorException("No se encontró el detalle correspondiente para el DNI_CAUSANTE con ID_CAUSANTE_PADRE = null.");
+            }
+          }
+
+          let persona = await manager.findOne(net_persona, { where: { n_identificacion: dni } });
+          if (!persona) {
+            persona = manager.create(net_persona, { n_identificacion: dni });
+            persona = await manager.save(persona);
+          }
+
+          const detallePersona = manager.create(net_detalle_persona, {
+            ID_PERSONA: persona.id_persona,                  
+            ID_CAUSANTE: causantePersona.id_persona,        
+            ID_CAUSANTE_PADRE: causantePersona.id_persona,  
+            ID_DETALLE_PERSONA: causanteDetallePersona.ID_DETALLE_PERSONA, 
+            ID_TIPO_PERSONA: tipoPersona,                   
+          });
+          const savedDetallePersona = await manager.save(detallePersona);
+
+          const banco = await manager.findOne(Net_Banco, { where: { cod_banco: codigoBanco } });
+          if (!banco) {
+            throw new BadRequestException(`No se encontró un banco con el código ${codigoBanco}`);
+          }
+
+          const personaPorBanco = manager.create(Net_Persona_Por_Banco, {
+            persona: persona,
+            banco: banco,
+            num_cuenta: numeroCuenta,
+            estado: 'ACTIVO',
+            fecha_activacion: new Date()
+          });
+          await manager.save(personaPorBanco);
+
+          const detalleBeneficioAfiliado = manager.create(Net_Detalle_Beneficio_Afiliado, {
+            ID_DETALLE_PERSONA: savedDetallePersona.ID_DETALLE_PERSONA,
+            ID_PERSONA: savedDetallePersona.ID_PERSONA,
+            ID_CAUSANTE: savedDetallePersona.ID_CAUSANTE,
+            ID_BENEFICIO: idBeneficio,
+            monto_primera_cuota: primerPago,
+            monto_ultima_cuota: ultimoPago,
+            fecha_calculo: fechaCalculo,
+            num_rentas_aplicadas: numeroRentas,
+            monto_por_periodo: montoPorPeriodo,
+            periodo_inicio: periodoInicio,
+            estado_solicitud: 'APROBADO'
+          });
+          await manager.save(detalleBeneficioAfiliado);
         });
-        const savedDetallePersona = await this.detallePersonaRepository.save(detallePersona);
-
-        const banco = await this.bancoRepository.findOne({ where: { cod_banco: codigoBanco } });
-        if (!banco) {
-          throw new BadRequestException(`No se encontró un banco con el código ${codigoBanco}`);
-        }
-
-        const personaPorBanco = this.personaPorBancoRepository.create({
-          persona: persona,
-          banco: banco,
-          num_cuenta: numeroCuenta,
-          estado: 'ACTIVO',
-          fecha_activacion: new Date()
-        });
-        await this.personaPorBancoRepository.save(personaPorBanco);
-
-        const detalleBeneficioAfiliado = this.detalleBeneficioAfiliadoRepository.create({
-          ID_DETALLE_PERSONA: savedDetallePersona.ID_DETALLE_PERSONA,
-          ID_PERSONA: savedDetallePersona.ID_PERSONA,
-          ID_CAUSANTE: savedDetallePersona.ID_CAUSANTE,
-          ID_BENEFICIO: idBeneficio,
-          monto_primera_cuota: primerPago,
-          monto_ultima_cuota: ultimoPago,
-          fecha_calculo: fechaCalculo,
-          num_rentas_aplicadas: numeroRentas,
-          monto_por_periodo: montoPorPeriodo,
-          periodo_inicio: periodoInicio,
-          estado_solicitud: 'APROBADO'
-        });
-        await this.detalleBeneficioAfiliadoRepository.save(detalleBeneficioAfiliado);
       }
       return { message: 'Datos cargados correctamente' };
     } catch (error) {

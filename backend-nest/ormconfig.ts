2d22d88d48461f9b3b2376430ebc98bc53568876
Yamir Alejandro Rodas Elvir <79547618+iamyare@users.noreply.github.com>
2025-03-24 18:56:01 -0600

    Actualizar configuración de conexión a la base de datos para usar Oracle en lugar de PostgreSQL

--- backend-nest/ormconfig.ts
+++ backend-nest/ormconfig.ts
@@ -2,18 +2,17 @@ import { DataSource } from 'typeorm';
 import * as dotenv from 'dotenv';
 
 dotenv.config({ path: 'development.env' });
-// La configuración de oracledb ya no es necesaria
+import * as oracledb from 'oracledb';
+oracledb.initOracleClient({ configDir: '', libDir: '', errorDir: '' });
 
 export const AppDataSource = new DataSource({
-  type: 'postgres',
-  url: 'postgresql://neondb_owner:npg_jpPEqoFy5s2e@ep-round-hat-a2i4jhkf-pooler.eu-central-1.aws.neon.tech/neondb',
-  ssl: true,
-  extra: {
-    ssl: {
-      rejectUnauthorized: false
-    }
-  },
+  type: 'oracle',
+  connectString: process.env.CONNECT_STRING,
+  port: parseInt(process.env.DATABASE_PORT, 10),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
   entities: [__dirname + '/src/**/*.entity{.ts,.js}'],
   migrations: [__dirname + '/src/migrations/*{.ts,.js}'],
   synchronize: false,
-});
+  
+}); 
